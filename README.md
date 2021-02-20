d04e65b3c7ada2e74b935b8f08819b028f81a0a0
Cory <coryhurst83@gmail.com>
2021-02-20 01:56:52 -0500

    trampoline new event loop

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Resources for brushing up:
  
  https://www.youtube.com/watch?v=F19R_M4Nay4
  
+ https://stackoverflow.com/questions/54254252/asyncio-vs-gevent
+ 
--- async_practice.py
+++ async_practice.py
@@ -1,37 +1,143 @@
 import asyncio
+import datetime
+import requests
+import traceback
+
+# random
+# async def main():
+#     await asyncio.sleep(1)
+#     print('hello')
+#
+# asyncio.run(main())
+#
+# loop = asyncio.new_event_loop()
+# asyncio.set_event_loop(loop)
+#
+#
+# loop = asyncio.get_event_loop()
+#
+#
+
+# def print_now():
+#     print(datetime.datetime.now())
+#
+#
+# loop.call_soon(print_now)
+# loop.call_soon(print_now)
+# loop.run_until_complete(asyncio.sleep(5))
+#
+# def trampoline(name = ""):
+#     print(name, end=" ")
+#     print_now()
+#     loop.call_later(0.5, trampoline, name)
+#
+# loop.call_soon(trampoline)
+# loop.call_later(8, print_now)
+#
+# loop.run_forever()
 
 
-async def main():
-    await asyncio.sleep(1)
-    print('hello')
+# TODO use async timing methods
+# timing syncronous code
+sync_list = []
+
+def searchApi():
+
+    url = "https://findtreatment.samhsa.gov/locator/listing"
+    sync_page = 0
+
+    try:
+        while True:
+            sync_page += 1
+            data = {"sType": "BOTH",
+                    "sAddr": "38.3507711,-85.9100089",
+                    "pageSize": "100",
+                    "page": {sync_page},
+                    "sort": "0"}
+
+            response = requests.post(url, data=data)
+            if(response.status_code == 200):
+                res = response.json()
+                sync_list.append(res["rows"])
+                if sync_page == res["totalPages"]:
+                    print("end")
+                    break
+    except Exception:
+        print(traceback.format_exc())
+
+start_sync = datetime.datetime.now()
+searchApi()
+end_sync = datetime.datetime.now()
+
+print(end_sync - start_sync)
 
 
-asyncio.run(main())
+# async seems faster want to store the result
+# ######################################
+#
+import aiohttp
+import json
+async_list = []
 
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
+async def fetch(session, url, data):
+    async with session.post(url, data=data) as response:
+        return await response.json()
 
+async def main(url, data):
+    async with aiohttp.ClientSession() as session:
+        return await fetch(session, url, data)
+
+page = 0
+
+url = "https://findtreatment.samhsa.gov/locator/listing"
+
+data = {"sType": "BOTH",
+        "sAddr": "38.3507711,-85.9100089",
+        "pageSize": "100",
+        "page": "1",
+        "sort": "0"}
+
+
+# grequests with gevents
+
+import grequests
+
+response = grequests.post(url, data=data)
+res = grequests.map([response])
+if(res[0].status_code == 200):
+    total_pages = res[0].json()["totalPages"]
+else:
+    raise Exception("Bad Request")
 
 loop = asyncio.get_event_loop()
 
+tupes = []
+while True:
 
-import datetime
-def print_now():
-    print(datetime.datetime.now())
+    page += 1
+    data = {"sType": "BOTH",
+            "sAddr": "38.3507711,-85.9100089",
+            "pageSize": "100",
+            "page": f"{page}",
+            "sort": "0"}
+    tupes.append((url, data))
+    if page >= total_pages:
+        break
+
+task = asyncio.gather(*[main(tupe[0], tupe[1]) for tupe in tupes])
+
+start_async = datetime.datetime.now()
+loop.run_until_complete(task)
+end_async = datetime.datetime.now()
+
+print(end_async - start_async)
 
 
-loop.call_soon(print_now)
-loop.call_soon(print_now)
-loop.run_until_complete(asyncio.sleep(5))
+rs = (grequests.post(u, data=d) for u, d in tupes)
 
-def trampoline(name = ""):
-    print(name, end=" ")
-    print_now()
-    loop.call_later(0.5, trampoline, name)
+results = grequests.map(rs)
 
-loop.call_soon(trampoline)
-loop.call_later(8, print_now)
-
-loop.run_forever()
-
+import json
+for each in results:
+    print(json.loads(each.text))
 
